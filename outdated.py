fe9f5bd264e61a13a1cda1a57354288871ed98aa
Eka <eka.abashidze@gmail.com>
2023-07-29 23:58:23 +0400

    refactor: add main function to the outdated program

--- outdated.py
+++ outdated.py
@@ -1,5 +1,3 @@
-date = input("Date: ")
-
 months = [
     "January",
     "February",
@@ -15,21 +13,25 @@ months = [
     "December"
 ]
 
-if '/' in date:
-    date = date.split("/")
-    orderedDate = [date[2], f"{int(date[0]):02}", f"{int(date[1]):02}"]
-    date = "/".join(orderedDate)
-
-for month in months:
-    if month in date:
-        date = date.split(", ")
-        monthConverted = date[0].split(" ")
-        day = monthConverted[1]
-        monthConverted = monthConverted[0]
-        if monthConverted == month:
-            monthFormatted = f"{months.index(monthConverted) + 1:02}"
-        orderedDate = [date[1], monthFormatted, day]
+def main():
+    date = input("Date: ")
+    
+    if '/' in date:
+        date = date.split("/")
+        orderedDate = [date[2], f"{int(date[0]):02}", f"{int(date[1]):02}"]
         date = "/".join(orderedDate)
-        print("ordered date:", date)
 
-print(date)
+    for month in months:
+        if month in date:
+            date = date.split(", ")
+            monthConverted = date[0].split(" ")
+            day = monthConverted[1]
+            monthConverted = monthConverted[0]
+            if monthConverted == month:
+                monthFormatted = f"{months.index(monthConverted) + 1:02}"
+            orderedDate = [date[1], monthFormatted, day]
+            date = "/".join(orderedDate)
+
+    print(date)
+
+main()
