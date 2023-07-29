d6fc0eb9417114f94cf55fc4d2d39519873b233d
Eka <eka.abashidze@gmail.com>
2023-07-29 22:01:35 +0400

    feat: learn custom module/functions importing

--- say.py
+++ say.py
@@ -1,5 +1,11 @@
 import cowsay
 import sys
+import sayings
+
+from sayings import goodbye
 
 if len(sys.argv) == 2:
     cowsay.dragon("Hello, " + sys.argv[1])
+    
+goodbye("Eka")
+sayings.hello("Eka")
