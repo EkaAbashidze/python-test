9daf128e12d0a0ee8f04a56e85f8794f3e53ab0c
Eka <eka.abashidze@gmail.com>
2023-07-30 01:17:36 +0400

    feat: create emojize program with emoji module

---
+++ emojize.py
@@ -0,0 +1,8 @@
+import emoji
+
+def main():
+    str = input("Input: ")
+    print("Output:", emoji.emojize(str))
+
+if __name__ == "__main__":
+    main()
--- outdated.py
+++ outdated.py
@@ -18,21 +18,21 @@ def main():
     
     if '/' in date:
         date = date.split("/")
-        orderedDate = [date[2], f"{int(date[0]):02}", f"{int(date[1]):02}"]
-        date = "-".join(orderedDate)
+        ordered_date = [date[2], f"{int(date[0]):02}", f"{int(date[1]):02}"]
+        date = "-".join(ordered_date)
         print(date)
         
     for month in months:
         if month in date:
             date = date.split(", ")
-            monthConverted = date[0].split(" ")
-            day = monthConverted[1]
-            monthConverted = monthConverted[0]
-            if monthConverted.lower() == month.lower():
-                monthFormatted = f"{months.index(monthConverted) + 1:02}"
+            month_converted = date[0].split(" ")
+            day = month_converted[1]s
+            month_converted = month_converted[0]
+            if month_converted.lower() == month.lower():
+                month_formatted = f"{months.index(month_converted) + 1:02}"
             try:
-                orderedDate = [date[1], monthFormatted, day]
-                date = "-".join(orderedDate)
+                ordered_date = [date[1], month_formatted, day]
+                date = "-".join(ordered_date)
                 print(date)
             except UnboundLocalError:
                 pass
