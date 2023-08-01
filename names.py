ea210775801bec0bfd2254330bbc438fc2b9c9a3
Eka <eka.abashidze@gmail.com>
2023-08-01 11:24:52 +0400

    feat: create csv file and dictionaries for the list

--- names.py
+++ names.py
@@ -1,5 +1,8 @@
-with open("names.txt", "r") as file:
-    lines = file.readlines()
-    
-for line in lines:
-    print("hello,", line.rstrip())
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in names:
+    print(f"Hello, {name}")
