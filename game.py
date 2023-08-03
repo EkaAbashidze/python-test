5585d6b5bbd11ccb2817cef1ec518f1bb2c98ac8
Eka <eka.abashidze@gmail.com>
2023-08-04 00:12:05 +0400

    feat: create professor program for user to answer calculations

--- game.py
+++ game.py
@@ -21,4 +21,4 @@ def main():
             exit()
 
 if __name__ == "__main__":
-    main()
+    main() 
---
+++ professor.py
@@ -0,0 +1,39 @@
+import random
+
+
+def main():
+    num = get_level()
+    generate_integer(num)
+
+def get_level():
+    while True:
+        n = int(input("Level: "))
+        if n == 1 or n == 2 or n == 3:
+            return n
+        else:
+            continue
+
+def generate_integer(level):
+    user_score = 0
+    for i in range(1, 11):
+        if level == 1:
+            X = random.choice(range(0, 9))
+            Y = random.choice(range(0, 9))
+        elif level == 2:
+            X = random.choice(range(10, 99))
+            Y = random.choice(range(10, 99))
+        elif level == 3:
+            X = random.choice(range(100, 999))
+            Y = random.choice(range(100, 999))
+        calculation = f'{X} + {Y} ='
+        answer = X + Y
+        user_answer = int(input(f'{calculation} '))
+        if user_answer != answer:
+            print("EEE")
+        else:
+            user_score = user_score + 1
+            continue
+    print(f'Your score: {user_score}')
+
+if __name__ == "__main__":
+    main()
