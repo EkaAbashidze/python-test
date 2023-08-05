74d97c97c8dcbdfa51e4a274fc116c3f48bca445
Eka <eka.abashidze@gmail.com>
2023-08-05 23:41:20 +0400

    feat: add price getting and converting functionality

--- bitcoin.py
+++ bitcoin.py
@@ -6,16 +6,13 @@ def main():
         n = float(sys.argv[1])
         response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         rate_usd = response.json()['bpi']['USD']['rate_float']
-        print(rate_usd)
+        cost_float = rate_usd * n
+        print(f"${cost_float:,.4f}")
     except ValueError:
-        print("Your input is not convertable")
+        print("Your input is not convertable - input ")
         sys.exit()
     except requests.RequestException:
-        pass
-        
-# print(f"${amount:,.4f}")
-
-
+        print("Data is unavailable right now, try again later")
 
 if __name__ == "__main__":
     main()
