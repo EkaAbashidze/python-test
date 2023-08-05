7c49d981a537cb22245b124637ec42f8e039ad8d
Eka <eka.abashidze@gmail.com>
2023-08-05 23:31:43 +0400

    feat: add input and valueerror functionality

--- bitcoin.py
+++ bitcoin.py
@@ -2,6 +2,16 @@ import sys
 import requests
 
 def main():
+    try:
+        n = float(sys.argv[1])
+        print(n)
+    except ValueError:
+        print("Your input is not convertable")
+        sys.exit()
+    except requests.RequestException:
+        pass
+        
+# print(f"${amount:,.4f}")
 
 
 
