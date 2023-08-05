f13a300810a67bb6c6f861fc427db13b1c5de1c8
Eka <eka.abashidze@gmail.com>
2023-08-05 23:37:31 +0400

    feat: add request for rest API and json formatting functionality

--- bitcoin.py
+++ bitcoin.py
@@ -4,7 +4,9 @@ import requests
 def main():
     try:
         n = float(sys.argv[1])
-        print(n)
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        rate_usd = response.json()['bpi']['USD']['rate_float']
+        print(rate_usd)
     except ValueError:
         print("Your input is not convertable")
         sys.exit()
