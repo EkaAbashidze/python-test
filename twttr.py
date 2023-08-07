438969da046518d027caedb0dead31c84de9cdd6
Eka <eka.abashidze@gmail.com>
2023-08-07 17:53:46 +0400

    refactor: modify old program to adjust with new requirements

--- twttr.py
+++ twttr.py
@@ -1,17 +1,12 @@
 def main():
-	word = input("Input: ")
-	new_word = ""
-	for char in word:
-		if char != "a" and char != "e" and char != "i" and char != "o" and char != "u" and char != "A" and char != "E" and char != "I" and char != "O" and char != "U" :
-			new_word += char
-	print(f"Output: {new_word}")
+    str = input("Input: ")
+    shorten(str)
 
-main()
-
-def main():
-    word = input("Input: ")
+def shorten(word):
     vowels = 'aeiou'
     new_word = ''.join(char for char in word if char.lower() not in vowels)
     print(f"Output: {new_word}")
+    
 
-main()
+if __name__ == "__main__":
+    main()
