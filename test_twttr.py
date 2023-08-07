e1f29039e05adccb6e2112ded8790480a64ae267
Eka <eka.abashidze@gmail.com>
2023-08-07 18:04:37 +0400

    feat: add assert tests to the test_twttr program and pass them

--- test_twttr.py
+++ test_twttr.py
@@ -1,4 +1,8 @@
 from twttr import shorten
 
 def test_twttr():
+    assert shorten("TWITTER") == "twttr"
     assert shorten("Twitter") == "twttr"
+    assert shorten("twitter") == "twttr"
+    assert shorten("Eka") == "k"
+    assert shorten("") == ""
--- twttr.py
+++ twttr.py
@@ -1,13 +1,12 @@
 def main():
     str = input("Input: ")
     word = shorten(str)
-    print(f"Output: {word.lower()}")
+    print(f"Output: {word}")
 
 def shorten(word):
     vowels = 'aeiou'
     new_word = ''.join(char for char in word if char.lower() not in vowels)
-    return new_word
-    
+    return new_word.lower()
 
 if __name__ == "__main__":
     main()
